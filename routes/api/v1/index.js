9b82a689524dadf67e15c0ba9456483f5bedd531
David Jm <danimax.com@gmail.com>
2022-07-23 00:23:15 -0500

    Added basic form, ajax jquery, base api

--- routes/api/v1/index.js
+++ routes/api/v1/index.js
@@ -4,4 +4,11 @@ module.exports = async function (fastify, opts) {
   fastify.get('/callback', async function (request, reply) {
     return 'this is an example'
   })
+  fastify.get('/keys-info/:client_id', async function (request, reply) {
+    const { client_id } = request.params;
+    return `this is an example ${client_id}`;
+  })
+  fastify.post('/save-keys', async function (request, reply) {
+    return reply.send(request.body);
+  })
 }
